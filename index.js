8d1791dd77dd7e3fa381ce72cccaaaa95ecb4990
Oscar <oscarkimani86@icloud.com>
2024-04-08 14:12:32 +0300

    string

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'GRACE HOPPER';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser[0].toUpperCase() + currentUser.slice(1) + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0).toUpperCase() + '!';
